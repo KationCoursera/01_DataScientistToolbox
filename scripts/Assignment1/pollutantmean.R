5785884786902129bfdb149c4e90177cc506ab4a
Jure Bordon <jure.bordon@gmail.com>
2014-12-13 19:53:01 +0100

    Assignment1/parts1-3

--- scripts/Assignment1/pollutantmean.R
+++ scripts/Assignment1/pollutantmean.R
@@ -13,7 +13,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
 
-  files <- list.files(paste(getwd(),directory, sep=""), full.names=TRUE)
+  files <- list.files(directory, full.names=TRUE)
   data <- numeric()
   
   for (i in id) {
